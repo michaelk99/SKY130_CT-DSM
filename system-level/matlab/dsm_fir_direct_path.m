fac4cbac78ca40147cc596bc195f5686a0bcf93f
Michael Köfinger <4404414+michaelk99@users.noreply.github.com>
2024-12-18 16:35:58 +0100

    system-level/matlab/dsm_fir_direct_path.m: extend FIR-DAC design by a ELD compensation path

--- system-level/matlab/dsm_fir_direct_path.m
+++ system-level/matlab/dsm_fir_direct_path.m
@@ -15,7 +15,7 @@
 % %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 clc; clear all; close all;
-addpath('/home/michael/tools/delsig/delsig/')
+addpath('/home/koefinger/tools/delsig/delsig/')
 addpath('./ctdsm/')
 addpath('./utility/')
 
@@ -33,6 +33,7 @@ M = nlev -1;
 delta = 2;
 FS = 2*(M/2*delta);
 N_FFT = ceil(fs/fres);
+eld = 0.5; % excess loop delay in units of Ts
 
 %% Stimulus (frequency domain plotting)
 fsignal = 18;
@@ -43,7 +44,17 @@ u = (10^(ampldBFS/20))*FS/2*sin(2*pi*fsignal./fs*n);
 %% Realize Mod (CT)
 ntf_synth = synthesizeNTF(ord, osr, optZ);
 ntf_ss = ss(ntf_synth);
-[ABCD_ct, tdac2] = realizeNTF_ct(ntf_synth, 'FF');
+[ABCD_ct, tdac2] = realizeNTF_ct(ntf_synth, 'FF', [0 1]+eld);
+
+% create dedicated input for direct path
+% if eld >=1 then realizeNTF_ct does this for us
+if eld > 0 && eld < 1
+    [Act, Bct, Cct, Dct] = partitionABCD(ABCD_ct);
+    Bct = [Bct zeros(ord,1)];
+    Dct = [0 Dct];
+    ABCD_ct = [[Act Bct];[Cct Dct]];
+    tdac2 = [tdac2; [0 1]+eld];
+end
 
 %% Modify coeff. for use with FIR DAC (Methods of Moments)
 ntaps = 4;
@@ -82,53 +93,69 @@ impl_dt_ntf = -impL1(ntf_synth,n_imp);
 % Determine pulse response of the prototype loop filter
 [Ac, Bc, Cc, Dc] = partitionABCD(ABCD_ct);
 sys_ct = ss( Ac, Bc, Cc, Dc);
-impl_ct = -pulse(sys_ct, [0 1], 1, n_imp, 0);
+tdac3 = [0 1].*ones(length(Dc),1)+eld;
+impl_ct = -pulse(sys_ct, tdac3, 1, n_imp, 1);
 impl_ct = squeeze(impl_ct);
+impl_ct = impl_ct(:,2:end);
+impl_ct_sum = sum(impl_ct,2); % sum v1 and v2 paths in case of direct path
 
 % Determine pulse response of the coefficient-tuned loop filter 
 % with FIR DAC
 [Ac, Bc, Cc, Dc] = partitionABCD(ABCD_ct_tuned);
 Bc(1,2) = -f0;
 sys_ct_tuned = ss( Ac, Bc, Cc, Dc);
-impl_ct_tuned = -pulse(sys_ct_tuned, [0 ntaps], 1, n_imp, 0);
+tdac4 = tdac3; tdac4(2,2) = ntaps+eld;
+impl_ct_tuned = -pulse(sys_ct_tuned, tdac4, 1, n_imp, 1);
 impl_ct_tuned = squeeze(impl_ct_tuned);
-
+impl_ct_tuned = impl_ct_tuned(:,2);
+% impl_ct_tuned_sum = sum(impl_ct_tuned,2); % sum v1 and v2 paths in case of direct path
+% impl_ct_tuned_sum = impl_ct_tuned(:,1);
 % Calc difference for first ntaps samples
-fc = impl_ct(2:ntaps,2)-impl_ct_tuned(2:ntaps,2);
+fc = impl_ct_sum-impl_ct_tuned;
+fc(fc<1e-10)=0;
+fc_plt = fc;
+fc = nonzeros(fc)
 
 % Plot
-% tvec = 0:n_imp;
-% fc_plt = [0 transpose(fc) zeros(1,n_imp-ntaps+1)];
-% figure;
-% hold on;
-% plot(tvec, impl_dt_ntf,'*-','LineWidth',1)
-% plot(tvec, impl_ct(:,2),'o-','LineWidth',1)
-% plot(tvec, impl_ct_tuned(:,2),'o-','LineWidth',1)
-% stem(tvec, fc_plt)
-% % plot(yy2(:,1,2),'o-')
+tvec = 0:n_imp;
+figure;
+hold on;
+plot(tvec, impl_dt_ntf,'*-','LineWidth',1)
+plot(tvec, impl_ct_sum,'o-','LineWidth',1)
+plot(tvec, impl_ct_tuned,'o-','LineWidth',1)
+stem(tvec, fc_plt)
+% plot(yy2(:,1,2),'o-')
 % xlim([0 n_imp])
-% ylim([0 max(impl_dt_ntf)])
-% set(gca,'FontSize',14)
-% title('Sampled Loop Filter Impulse Responses','Fontsize', 14)
-% legend('NTF-Prototype', 'CT-Prototype', 'CT FIR-tuned','Coeff. of Fc(z)','Fontsize', 14)
-% ylabel('p(t)','Fontsize', 14)
-% xlabel('t in s','Fontsize', 14)
-% grid on;
+ylim([0 max(impl_dt_ntf)])
+set(gca,'FontSize',14)
+title('Sampled Loop Filter Impulse Responses','Fontsize', 14)
+legend('NTF-Prototype', 'CT-Prototype', 'CT FIR-tuned','Coeff. of Fc(z)','Fontsize', 14)
+ylabel('p(t)','Fontsize', 14)
+xlabel('t in s','Fontsize', 14)
+grid on;
 
 %% Add direct path input to state space description of CT prototype
 [Ac, Bc, Cc, Dc] = partitionABCD(ABCD_ct_tuned);
 Bc_fir = [1 -1 0; 0 0 0; 0 0 0; 0 0 0];
 Dc_fir = [0 0 -1];
+% Bc_fir = [1 -1 0 0; 0 0 0 0; 0 0 0 0; 0 0 0 0];
+% Dc_fir = [0 0 -1 Dc(3)];
 
 %% FIR DAC State Space Descr. w/ compensation DAC
 Afir  = sys_fir.A;
 %Bfir = ss_fir.B./ss_fir.B(1);
 Bfir = sys_fir.B;
 % Cfir = [ss_fir.C.*ss_fir.B(1); ss_fir.C.*ss_fir.B(1)];
-Cfir = [sys_fir.C; fc'];
-Dfir = [sys_fir.D; 0];
-% Cfir = [sys_fir.C; 0 fc(2:end)'];
-% Dfir = [sys_fir.D; fc(1)];
+if length(fc) < ntaps
+    Cfir = [sys_fir.C; fc'];
+    Dfir = [sys_fir.D; 0];
+elseif length(fc) == ntaps
+    % direct path requiered
+    Cfir = [sys_fir.C; fc(2:end)'];
+    Dfir = [sys_fir.D; fc(1)];
+else
+    error("too many compensation coefficients");
+end
 sys_fir_simo = ss(Afir, Bfir, Cfir, Dfir, 1);
 figure; impulse(sys_fir_simo); title('Impulse Response of F(z) and Fc(z)')
 figure; bode(sys_fir_simo); title('Bode Plot of F(z) + Fc(z)')
@@ -136,7 +163,7 @@ figure; bode(sys_fir_simo); title('Bode Plot of F(z) + Fc(z)')
 %% Assemble combined system
 % INFO: [Ac, Bc, Cc, Dc] = partitionABCD(ABCD_ct_tuned);
 sys_c = ss(Ac, Bc_fir, Cc, Dc_fir);
-sys_d = c2d(sys_c,1);
+sys_d = c2d(sys_c,1, 'zoh');
 
 Ad = sys_d.A; 
 Bd1 = sys_d.B(:,1); Bd23 = [sys_d.B(:,2)'; [0 0 0 0]]';
@@ -147,8 +174,10 @@ Acomb = [Ad Bd23*Cfir;
         zeros(ntaps-1,ord) Afir]
 Bcomb = [Bd1 Bd23*Dfir;
         zeros(ntaps-1,1) Bfir]
+% Bcomb = [Bcomb zeros(ord+ntaps-1,1)]
 Ccomb = [Cd Dd23*Cfir]
 Dcomb = [Dd1 Dd23*Dfir]
+% Dcomb = [Dd1 Dd23*Dfir sys_d.D(1,4)]
 ABcomb = [Acomb Bcomb];
 CDcomb = [Ccomb Dcomb];
 
@@ -160,19 +189,31 @@ tvec = 0:n_imp;
 impl_dt_ntf = -impL1(ntf_synth,n_imp);
 
 [Adcomb, Bdcomb, Cdcomb, Ddcomb] = partitionABCD(ABCDcomb);
-sys_fir_sim = ss(Adcomb, Bdcomb, Cdcomb, Ddcomb, 1);
+
+% Convert discrete time system into Matlab SS model and use Matlab function
+% to calculate the impulse response
+sys_fir_sim = ss(Adcomb, Bdcomb, Cdcomb, Ddcomb,1);
 impl_fir_sim = -impulse(sys_fir_sim,tvec);
 impl_fir_sim = squeeze(impl_fir_sim);
+impl_fir_sim = [zeros(1,ceil(eld)); impl_fir_sim(1:end-ceil(eld),2)];
 
+% GIVES WRONG RESULTS, 'tustin' is probably the wrong method, but 'zoh' is
+% not allowed here?!
+% Convert discrete time Matlab SS model into a continuous time model
+% and use delsig toolbox to calculate the impulse response
+% sys_fir_sim_ct = d2c(sys_fir_sim, "tustin");
+% tdac5 = [0 1].*ones(length(Ddcomb),1)+eld;
+% impl_fir_sim_ct = -pulse(sys_fir_sim_ct,tdac5, 1, n_imp, 1);
+% impl_fir_sim_ct = impl_fir_sim_ct(:,2);
 
-fc_plt = [0 transpose(fc) zeros(1,n_imp-ntaps+1)];
 figure;
 hold on;
 plot(tvec, impl_dt_ntf,'*-','LineWidth',1.2)
-plot(tvec, impl_ct(:,2),'o-','LineWidth',1.2)
-plot(tvec, impl_ct_tuned(:,2),'o-','LineWidth',1.2)
+plot(tvec, impl_ct_sum,'o-','LineWidth',1.2)
+plot(tvec, impl_ct_tuned,'o-','LineWidth',1.2)
 stem(tvec, fc_plt,'LineWidth',1)
-plot(tvec, impl_fir_sim(:,2),'o--','LineWidth',1.2)
+plot(tvec, impl_fir_sim,'o--','LineWidth',1.2)
+% plot(tvec, impl_fir_sim_ct+abs(impl_fir_sim_ct(1)),'*--','LineWidth',1.2)
 xlim([0 n_imp])
 ylim([0 max(impl_dt_ntf)])
 set(gca,'FontSize',14)
