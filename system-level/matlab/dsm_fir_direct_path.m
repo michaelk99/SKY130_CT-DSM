6c014a7ef741ff6c748b8a47dde90464ac05d712
Michael Köfinger <4404414+michaelk99@users.noreply.github.com>
2025-03-11 10:52:30 +0100

    system-level/matlab/dsm_fir_direct_path.m: use pulse_fir.m, prepare different FIR filter shapes, add delay if ELD=1, add comments and debug outputs

--- system-level/matlab/dsm_fir_direct_path.m
+++ system-level/matlab/dsm_fir_direct_path.m
@@ -14,7 +14,8 @@
 % limitations under the License.
 % %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-clc; clear all; close all;
+clc; clear all; 
+close all;
 addpath('/home/koefinger/tools/delsig/delsig/')
 addpath('./ctdsm/')
 addpath('./utility/')
@@ -33,7 +34,9 @@ M = nlev -1;
 delta = 2;
 FS = 2*(M/2*delta);
 N_FFT = ceil(fs/fres);
-eld = 0.5; % excess loop delay in units of Ts
+% Note: delsig is used for simulation, so only a ELD of 1*Ts can be modeled
+% Expect slight deviations of the NTF in the PSD for 0 < ELD < 1*Ts
+eld = 1; % excess loop delay in units of Ts
 
 %% Stimulus (frequency domain plotting)
 fsignal = 18;
@@ -59,28 +62,83 @@ end
 %% Modify coeff. for use with FIR DAC (Methods of Moments)
 ntaps = 4;
 [Ac, Bc, Cc, Dc] = partitionABCD(ABCD_ct);
+
+% [1] S. Pavan, "Continuous-Time Delta-Sigma Modulator Design Using the Method of Moments," 
+% in IEEE Transactions on Circuits and Systems I: Regular Papers, vol. 61, no. 6, pp. 1629-1637, June 2014
 c4_fir = Cc(4);
 c3_fir = Cc(3)+(Cc(4)*(ntaps-1))/2;
 c2_fir = Cc(2)+(6*Cc(3)*(ntaps-1)+Cc(4)*(ntaps.^2-3*ntaps+2))/12;
 c1_fir = Cc(1)+(12*Cc(2)*(ntaps-1)+2*Cc(3)*(ntaps.^2-3*ntaps+2)-Cc(4)*(ntaps.^2-2*ntaps+1))/24;
 Cc_fir = [c1_fir c2_fir c3_fir c4_fir];
-ABCD_ct_tuned = [[Ac Bc];[Cc_fir Dc]];
+% ABCD_ct_tuned = [[Ac Bc];[Cc_fir Dc]];
 
 %% FIR DAC State Space Descr.
-f0 = 1/(ntaps);
-num_fir = f0.*ones(1,ntaps);
+shape = 'rect';
+z = tf('z',1);
+
+% [2] A. Abdelaal, M. Pietzko, M. A. Mokhtar, J. G. Kauffman and M. Ortmanns, 
+% "FIR Filter with Symmetric Non-Equal Coefficients for CT Delta-Sigma Modulators," 
+% 2021 IEEE International Symposium on Circuits and Systems (ISCAS), Daegu, Korea, 2021
+H_fir_rect = (1-z^(-ntaps))/(ntaps*(1-z^(-1)));
+
+h_fir_rect = ones(1,ntaps)./ntaps;
+if strcmp(shape,'rect') == true
+    h_fir = h_fir_rect;
+    % ntaps_ = ntaps;
+    % H_fir = H_fir_rect;
+elseif strcmp(shape,'triang') == true
+    h_fir = triang(ntaps)'./sum(triang(ntaps));
+    % ntaps_ = ntaps;
+
+    % num_fir_1 = ones(1,ntaps-1)./(ntaps-1);
+    % num_fir_2 = ones(1,ntaps-2)./(ntaps-2);
+    % num_fir = conv(num_fir_1, num_fir_2);
+
+    % [2]:
+    % H_fir = H_fir_rect*H_fir_rect;
+    % ntaps_ = 2*ntaps-1;
+elseif strcmp(shape, 'hann') == true
+    h_fir = hann(ntaps)'./sum(hann(ntaps));
+    % ntaps_ = ntaps;
+elseif strcmp(shape, 'hamming') == true
+    h_fir = hamming(ntaps)'./sum(hamming(ntaps));
+    % ntaps_ = ntaps;
+elseif strcmp(shape, 'gauss') == true
+    h_fir = gausswin(ntaps)'./sum(gausswin(ntaps));
+    % ntaps_ = ntaps;
+else
+    error("unsupported filter shape!");
+end
+
+% h_fir = impulse(H_fir, ntaps_-1)';
 denom_fir = zeros(1,ntaps);
 denom_fir(1) = 1;
-tf_fir = tf(num_fir, denom_fir,1);
-Adc = dcgain(tf_fir);
-% tf_fir = tf_fir/Adc; 
-sys_fir = ss(tf_fir);
+H_fir = tf(h_fir, denom_fir, 1);
+sys_fir = ss(H_fir);
 
 % Bugfix
 if ntaps == 2
     sys_fir.C = sys_fir.B;
     sys_fir.B = 1;
 end
+
+% Calc moments of FIR-filtered NRZ pulse
+mom_fir_rect = zeros(1,ord);
+mom_fir = zeros(1,ord);
+for i=0:ord-1
+    mom_fir_rect(i+1) = moment(h_fir_rect, i);
+    mom_fir(i+1) = moment(h_fir, i);
+end
+
+% Adjust coefficients using MoM, based on moments of the filtered DAC pulse shape [1]
+c3_fir = Cc_fir(3) + Cc_fir(4)*(mom_fir(2)-mom_fir_rect(2));
+c2_fir = Cc_fir(2) + Cc_fir(3)*(mom_fir(2)-mom_fir_rect(2)) + Cc_fir(4)*(mom_fir(2)*(mom_fir(2)-mom_fir_rect(2))+(mom_fir_rect(3)-mom_fir(3))./2);
+c1_fir = Cc_fir(1) + Cc_fir(2)*(mom_fir(2)-mom_fir_rect(2)) + Cc_fir(3)*(mom_fir(2)*(mom_fir(2)-mom_fir_rect(2))+(mom_fir_rect(3)-mom_fir(3))./2) + Cc_fir(4)*(mom_fir(2)*(mom_fir(2).^2-mom_fir(2)*mom_fir_rect(2)-mom_fir(3)) + mom_fir(2)*mom_fir_rect(3)./2 + mom_fir(3)*mom_fir_rect(2)./2 + (mom_fir(4)-mom_fir_rect(4))./6);
+
+Cc_fir_ = [c1_fir c2_fir c3_fir c4_fir];
+
+ABCD_ct_tuned = [[Ac Bc];[Cc_fir_ Dc]];
+sys_lf = ss(Ac, Bc, Cc_fir, Dc);
 
 % Plots
 figure; impulse(sys_fir); title('Impulse Response of FIR Filter')
@@ -96,25 +154,31 @@ sys_ct = ss( Ac, Bc, Cc, Dc);
 tdac3 = [0 1].*ones(length(Dc),1)+eld;
 impl_ct = -pulse(sys_ct, tdac3, 1, n_imp, 1);
 impl_ct = squeeze(impl_ct);
-impl_ct = impl_ct(:,2:end);
+impl_ct = impl_ct(:,2:end);   % discard impulse response from ct-input u
 impl_ct_sum = sum(impl_ct,2); % sum v1 and v2 paths in case of direct path
 
 % Determine pulse response of the coefficient-tuned loop filter 
 % with FIR DAC
 [Ac, Bc, Cc, Dc] = partitionABCD(ABCD_ct_tuned);
-Bc(1,2) = -f0;
 sys_ct_tuned = ss( Ac, Bc, Cc, Dc);
-tdac4 = tdac3; tdac4(2,2) = ntaps+eld;
-impl_ct_tuned = -pulse(sys_ct_tuned, tdac4, 1, n_imp, 1);
-impl_ct_tuned = squeeze(impl_ct_tuned);
-impl_ct_tuned = impl_ct_tuned(:,2);
+impl_ct_tuned = pulse_fir(h_fir, sys_ct_tuned, eld, 1);
+impl_ct_tuned = impl_ct_tuned(:,1);
+% compare with DT system
+sys_dt_tuned = c2d(sys_ct_tuned,1,'zoh');
+sys_dt_tuned_fir = series(sys_fir, sys_dt_tuned, 1, 2);
+impl_dt_tuned = -impulse(sys_dt_tuned_fir, n_imp);
+
 % impl_ct_tuned_sum = sum(impl_ct_tuned,2); % sum v1 and v2 paths in case of direct path
 % impl_ct_tuned_sum = impl_ct_tuned(:,1);
 % Calc difference for first ntaps samples
 fc = impl_ct_sum-impl_ct_tuned;
+fc
 fc(fc<1e-10)=0;
 fc_plt = fc;
 fc = nonzeros(fc)
+if length(fc) > ntaps
+    fc = fc(1:ntaps);
+end
 
 % Plot
 tvec = 0:n_imp;
@@ -123,13 +187,15 @@ hold on;
 plot(tvec, impl_dt_ntf,'*-','LineWidth',1)
 plot(tvec, impl_ct_sum,'o-','LineWidth',1)
 plot(tvec, impl_ct_tuned,'o-','LineWidth',1)
+plot(tvec, impl_dt_tuned,'o--','LineWidth',1)
 stem(tvec, fc_plt)
 % plot(yy2(:,1,2),'o-')
 % xlim([0 n_imp])
 ylim([0 max(impl_dt_ntf)])
 set(gca,'FontSize',14)
-title('Sampled Loop Filter Impulse Responses','Fontsize', 14)
-legend('NTF-Prototype', 'CT-Prototype', 'CT FIR-tuned','Coeff. of Fc(z)','Fontsize', 14)
+titleStr = sprintf('Sampled Loop Filter Impulse Responses (nTaps = %d, ELD = %.1d Ts)', ntaps, eld);
+title(titleStr,'Fontsize', 14)
+legend('NTF-Prototype', 'CT-Prototype', 'CT FIR-tuned','DT FIR-tuned (only valid for ELD=0)', 'Coeff. of Fc(z)','Fontsize', 14)
 ylabel('p(t)','Fontsize', 14)
 xlabel('t in s','Fontsize', 14)
 grid on;
@@ -140,7 +206,15 @@ Bc_fir = [1 -1 0; 0 0 0; 0 0 0; 0 0 0];
 Dc_fir = [0 0 -1];
 % Bc_fir = [1 -1 0 0; 0 0 0 0; 0 0 0 0; 0 0 0 0];
 % Dc_fir = [0 0 -1 Dc(3)];
+print("DSM SS")
+[[Ac Bc_fir];[Cc, Dc_fir]]
 
+%% IF ELD = 1, delay input to FIR by one sample
+if eld == 1
+    tf_fir = tf([0 1], [1 0], 1);
+    sys_delay = ss(tf_fir);
+    sys_fir_delay = series(sys_delay, sys_fir);
+end
 %% FIR DAC State Space Descr. w/ compensation DAC
 Afir  = sys_fir.A;
 %Bfir = ss_fir.B./ss_fir.B(1);
@@ -159,6 +233,9 @@ end
 sys_fir_simo = ss(Afir, Bfir, Cfir, Dfir, 1);
 figure; impulse(sys_fir_simo); title('Impulse Response of F(z) and Fc(z)')
 figure; bode(sys_fir_simo); title('Bode Plot of F(z) + Fc(z)')
+print("FIR Filter")
+[[Afir Bfir];[Cfir, Dfir]]
+
 
 %% Assemble combined system
 % INFO: [Ac, Bc, Cc, Dc] = partitionABCD(ABCD_ct_tuned);
@@ -197,7 +274,7 @@ impl_fir_sim = -impulse(sys_fir_sim,tvec);
 impl_fir_sim = squeeze(impl_fir_sim);
 impl_fir_sim = [zeros(1,ceil(eld)); impl_fir_sim(1:end-ceil(eld),2)];
 
-% GIVES WRONG RESULTS, 'tustin' is probably the wrong method, but 'zoh' is
+% GIVES WRONG RESULTS, 'tustin' (bilinear trafo) is the wrong method, but 'zoh' is
 % not allowed here?!
 % Convert discrete time Matlab SS model into a continuous time model
 % and use delsig toolbox to calculate the impulse response
@@ -235,7 +312,7 @@ v1 = simulateDSM(u, ABCD_ct_sim, nlev);
 [v3,xn3,xmax3,y3] = simulateDSM(u, ABCDcomb, nlev);
 plotInternalSignals(1, fsignal, 1, [xn3(1,:); xn3(2,:); xn3(3,:); xn3(4,:)], ["Output Int. 1", "Output Int. 2", "Output Int. 3", "Output Int. 4"], false)
 xlim = xmax3;
-xlim(1:4) = 2.*[0.6 1 1 1];
+xlim(1:4) = 2.*[1 1 1 1];
 [ABCDcomb_scaled, umax, Tscale] = scaleABCD(ABCDcomb, 2, 0, xlim');
 msa = 20*log10(umax/(FS/2));
 [v4,xn4,xmax4,y4] = simulateDSM(u, ABCDcomb_scaled, nlev);
